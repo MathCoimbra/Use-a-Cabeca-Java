f0c31352a1df1b5ca5ed012f6cb916de66c27ddc
MathCoimbra <matheuscoimbra35@gmail.com>
2022-07-11 21:48:49 -0300

    adicionado exercicio do código da piscina

--- src/com/PoolPuzzleOne/PoolPuzzleOne.java
+++ src/com/PoolPuzzleOne/PoolPuzzleOne.java
@@ -3,26 +3,26 @@ package com.PoolPuzzleOne;
 class PoolPuzzleOne {
 	public static void main(String[] args) {
 		
-		int x = 0;
+		int x = 0; 
 		
-		while () {
-			
+		while (x<4) {
+			System.out.print("a");
 			if(x<1) {
-				
+				System.out.print(" ");
 			}
-			
-			if() {
-				
-				
+			System.out.print("n");
+			if(x>1) {
+				System.out.print(" oyster");
+				x=x+2;
 			}
 			if(x==1) {
-				
+				System.out.print("noys");
 			}
-			if() {
-				
+			if(x<1) {
+				System.out.print("oise");
 			}
 			System.out.println("");
-			
+			x=x+1;
 		}
 	}
 }
