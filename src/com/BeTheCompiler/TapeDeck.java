d637a748c0b6ef5c88cdaaca5e55cc2a674c32a4
MathCoimbra <matheuscoimbra35@gmail.com>
2022-12-14 21:11:39 -0300

    feat: add exercise A from Be The Compiler

---
+++ src/com/BeTheCompiler/TapeDeck.java
@@ -0,0 +1,28 @@
+package com.BeTheCompiler;
+
+public class TapeDeck {
+
+    boolean canRecord = false;
+
+    void playTape(){
+        System.out.println("tape playing");
+    }
+
+    void recordTape() {
+        System.out.println("tape recording");
+    }
+}
+
+class TapeDeckTestDrive {
+    public static void main(String[] args) {
+
+        TapeDeck t = new TapeDeck();
+
+        t.canRecord = true;
+        t.playTape();
+
+        if (t.canRecord) {
+            t.recordTape();
+        }
+    }
+}
