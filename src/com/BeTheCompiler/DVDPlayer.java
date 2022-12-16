be65883aa7e19d71ba97a54ebb0dd3c90775c110
MathCoimbra <matheuscoimbra35@gmail.com>
2022-12-15 21:25:50 -0300

    feat: add exercise B from Be The Compiler

---
+++ src/com/BeTheCompiler/DVDPlayer.java
@@ -0,0 +1,28 @@
+package com.BeTheCompiler;
+
+public class DVDPlayer {
+
+    boolean canRecord = false;
+
+    void playDVD(){
+        System.out.println("DVD playing");
+    }
+
+    void recordDVD() {
+        System.out.println("DVD recording");
+    }
+}
+
+class DVDPlayerTestDrive {
+    public static void main(String[] args) {
+
+        DVDPlayer d = new DVDPlayer();
+
+        d.canRecord = true;
+        d.playDVD();
+
+        if (d.canRecord) {
+            d.recordDVD();
+        }
+    }
+}
